fe9ef52e1d1903f171094398b2253ab268b876b8
if0balino <balino.ical@gmail.com>
2021-11-12 04:48:37 +0700

    types

---
+++ types-and-coercion/ex.js
@@ -0,0 +1,14 @@
+//primitive types
+
+var value
+value = "ical"
+// value = 19
+// value = true
+// value = { nama: "ical", umur: 19 }
+// value = Symbol()
+// //-------------------------------------------------
+// value = null
+// value = [1, 2, 3] //subtype of object
+// value = function () { } //subtype of object
+
+console.log(typeof value)
